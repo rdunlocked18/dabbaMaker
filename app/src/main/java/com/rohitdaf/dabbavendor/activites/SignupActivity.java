1b4d9d1e4026afa7f8a8f3346eeb0dc4126a1d6b
Jeeeet15 <jeetpalan123@gmail.com>
2020-07-21 19:02:39 +0530

    login signup

--- app/src/main/java/com/rohitdaf/dabbavendor/activites/SignupActivity.java
+++ app/src/main/java/com/rohitdaf/dabbavendor/activites/SignupActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.rohitdaf.dabbavendor.R;
+import com.rohitdaf.dabbavendor.databinding.ActivityLoginBinding;
 import com.rohitdaf.dabbavendor.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class SignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         activitySignupBinding = ActivitySignupBinding.inflate(getLayoutInflater());
         View view = activitySignupBinding.getRoot();
         setContentView(view);
